f5c426b0c5ca6deb2687f4d6e1b8cea29c294cb3
Terrence Chung <terrencechung35@gmail.com>
2023-01-19 18:38:42 -0800

    lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => console.log("The Dom has loaded"));
+
+//Use the event's callback function to target the paragraph element with id="text" and replace the text with "This is really cool!"
+const text = document.getElementById('text');
+
+document.addEventListener("DOMContentLoaded", () => text.textContent = 'This is really cool!');
